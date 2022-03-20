2d55eca967d08164a7e4b19427f3cdf4b936c862
Shrey <shrey9april@gmail.com>
2022-03-20 16:32:25 +0530

    Add: linked-list generator fn

--- exercises/linked-list/index.js
+++ exercises/linked-list/index.js
@@ -176,6 +176,15 @@ class LinkedList {
       counter++
     }
   }
+
+  *[Symbol.iterator]() {
+    let node = this.head
+
+    while (node) {
+      yield node
+      node = node.next
+    }
+  }
 }
 
 module.exports = { Node, LinkedList }
